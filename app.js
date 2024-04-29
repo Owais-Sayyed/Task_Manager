2d23c7942ef909eec74ea2f170835c486abcf5e0
Owais Sayyed <mowais.sayyed@gmail.com>
2024-04-29 14:15:17 +0530

    Complete Commit

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-const port = 3000;
+require("dotenv").config();
+const notFound = require("./middleware/notfound");
+const errorHandlerMiddleware = require("./middleware/error");
+const connectDB = require("./db/connect");
+const port = process.env.PORT || 3000;
 
 //middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
 
 app.use("/api/v1/tasks", tasks);
-app.listen(port, console.log(`server listen ${port}`));
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server listen ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
